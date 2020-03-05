c7829dc5e08c191c990dfe858f8e7f3163de793a
James Shen <james.shen@guidebee.com>
2020-03-05 08:55:23 +0800

    tutorial01

---
+++ tutorial.h
@@ -0,0 +1,19 @@
+//
+// Created by Jing SHEN on 2/3/20.
+//
+
+#ifndef FFMPEG_TUTORIAL_TUTORIAL_H
+#define FFMPEG_TUTORIAL_TUTORIAL_H
+#if defined (__cplusplus)
+extern "C" {
+#endif
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+
+#include <cstdio>
+
+#if defined (__cplusplus)
+}
+#endif
+#endif //FFMPEG_TUTORIAL_TUTORIAL_H
