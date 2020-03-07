553e4634865fbf0a833cfaa617a37ce87c62bf60
James Shen <james.shen@guidebee.com>
2020-03-07 16:28:15 +0800

    refactor ,seperate include directory

---
+++ include/fact.hpp
@@ -0,0 +1,11 @@
+//
+// Created by Jing SHEN on 7/3/20.
+//
+
+#ifndef FFMPEG_TUTORIAL_FACT_HPP
+#define FFMPEG_TUTORIAL_FACT_HPP
+
+
+unsigned int factorial(unsigned int);
+
+#endif //FFMPEG_TUTORIAL_FACT_HPP
--- src/fact.cpp
+++ src/fact.cpp
@@ -2,7 +2,7 @@
 // Created by Jing SHEN on 7/3/20.
 //
 
-#include "fact.hpp"
+#include "../include/fact.hpp"
 unsigned int factorial( unsigned int number ) {
     return number <= 1 ? number : factorial(number-1)*number;
 }
